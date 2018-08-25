c0a8a9e948c1fe2a18536506e81da1390338fc40
Rigoberto Campos Vega <rigobertotr98@gmail.com>
2018-08-25 22:48:46 +0000

    adding routes header footer bootstrap

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app = express();
+
+var albunsRoutes = require("./routes/albuns.js");
+var indexRoutes = require("./routes/index.js");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+
+app.use(function(req, res, next){
+    //res.locals.user = req.user;
+    next();
+});
+
+app.use("/albuns",albunsRoutes);
+app.use(indexRoutes);
+
+app.listen(process.env.PORT, process.env.IP);
