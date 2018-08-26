fe2885ad7ed094ba9818e7a7a61a089c47492abf
Rigoberto Campos Vega <rigobertotr98@gmail.com>
2018-08-26 00:43:59 +0000

    working whit api, dynamic data

--- routes/albuns.js
+++ routes/albuns.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var router = express.Router();
+var request = require("request");
 
 router.get("/", function(req, res){
-    res.render("albuns/index.ejs");
+    request("https://itunes.apple.com/us/rss/topalbums/limit=5/json", function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var data = JSON.parse(body);
+            var results = data["feed"]["entry"];
+            console.log(results["0"]["title"]);
+            res.render("albuns/index.ejs", {results: results});
+        }
+    });
 });
 
 module.exports = router;
